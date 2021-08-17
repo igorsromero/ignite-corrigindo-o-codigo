b3ce6d6680473b265863a19f7b091054e43419ac
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-08-17 17:28:08 -0300

    Conclusão do Desafio 03.

---
+++ README.md
@@ -0,0 +1,32 @@
+# Ignite - Corrigindo o Código
+
+Api desenvolvida como desafio do primeiro capitulo do curso Ignite Nodejs.
+
+Durante o desenvolvimento foi colocado em pratica conceitos de HTTP, Middleware e Express.
+
+## Sobre o projeto
+<details>
+<summary>Saiba mais</summary>
+Nesse desafio, temos uma aplicação Node.js que está em processo de desenvolvimento mas que já possui os testes necessários para fazer toda a validação dos requisitos (você não deve mexer nos testes).
+Após algumas alterações no código da aplicação, parte dos testes deixaram de passar e agora só você pode resolver esse problema. Bora lá? 🚀
+
+</details>
+
+<details>
+<summary>Para clonar o projeto</summary>
+
+Clonar o projeto
+```
+git clone https://github.com/igorsromero/ignite-corrigindo-o-codigo.git
+```
+
+Instalar as depêndencias
+```
+yarn install
+```
+
+Executar os testes
+```
+yarn test
+```
+</details>
--- src/index.js
+++ src/index.js
@@ -23,17 +23,22 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
+  }
+  
+  if(updatedRepository.likes != undefined) {
+    updatedRepository.likes = repositories[repositoryIndex].likes
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
@@ -45,16 +50,16 @@ app.put("/repositories/:id", (request, response) => {
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
-
+ 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
-
-  if (repositoryIndex > 0) {
+  console.log(repositoryIndex);
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   repositories.splice(repositoryIndex, 1);
 
-  return response.status(204).send();
+  return response.sendStatus(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -68,7 +73,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
